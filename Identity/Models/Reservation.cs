d9eb305da3b02a664435aabc195915713b64c571
Bojo <bojidar.sivinov@paisii.info>
2024-03-25 23:28:40 +0200

    Made the last model for Reservation.

---
+++ Identity/Models/Reservation.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Identity.Models
+{
+    public class Reservation
+    {
+        [Required]
+        public string ReservedRoom {  get; set; }
+        [Required]
+        public string UserReservation {  get; set; }
+        [Required]
+        public List<string> ClientsUsingRoom { get; set; }
+        [Required]
+        public string SettlementDate { get; set; }
+        [Required]
+        public string LeavingDate { get; set; }
+        [Required]
+        public bool BreakfastIncluded { get; set; }
+        [Required]
+        public bool AllInclusive { get; set; }
+        [Required]
+        public double Price { get; set; }
+    }
+}
